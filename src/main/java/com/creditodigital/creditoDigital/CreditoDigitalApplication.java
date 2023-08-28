996d08c931207a3fc2635fbd623e47e9d13f3b63
Gerson Correia Lima Neto <gerson.correia@hotmail.com>
2023-08-28 12:34:58 -0500

    subindo projeto

---
+++ src/main/java/com/creditodigital/creditoDigital/CreditoDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.creditodigital.creditoDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditoDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/creditodigital/creditoDigital/CreditoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditodigital.creditoDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
